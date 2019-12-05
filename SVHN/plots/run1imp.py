f19c0d0f3209ab799c9624ce5f58af58b5c6f55f
cflamant <cflamant@gmail.com>
2019-12-05 02:44:51 -0500

    CURL no cheating test

---
+++ SVHN/plots/run1imp.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+fig, ax = plt.subplots(1,1)
+supacc = np.load('0015.npy')
+curlacc = np.load('04-0015.npy')
+supacc2 = np.load('imp-0015.npy')
+curlacc2 = np.load('imp-04-0015.npy')
+
+supbatches = np.arange(supacc.shape[0])*1000
+curlbatches = np.arange(curlacc.shape[0])*1000 + supbatches[-1]
+supbatches2= np.arange(supacc2.shape[0])*1000
+curlbatches2= np.arange(curlacc2.shape[0])*1000 + supbatches2[-1]
+
+ax.plot(supbatches, supacc, linestyle='--', c='C1')
+ax.plot([supbatches[-1],curlbatches[0]], [supacc[-1],curlacc[0]], c='b' )
+run1 = ax.plot(curlbatches, curlacc, c='C1', label='110 labeled, 2930 unlabeled')
+ax.plot(supbatches2, supacc2, linestyle='--', c='C2')
+run2 = ax.plot(curlbatches2, curlacc2, c='C2', label='110 labeled, 2930 unlabeled no cheat')
+ax.plot([supbatches2[-1],curlbatches2[0]], [supacc2[-1],curlacc2[0]], c='b', label='CURL')
+ax.set_title("Boost from CURL")
+ax.set_xlabel("supervised batches")
+ax.set_ylabel("Test Accuracy")
+ax.legend()
+fig.savefig('curlboost-imp.pdf', bbox_inches='tight')
+plt.show()
+
